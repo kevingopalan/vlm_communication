cb7d9579354c21551c6a0b556e83271c636a047e
Kevin Gopalan <kevin.gopalan@vestal.k12.ny.us>
2025-07-21 17:26:39 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'vlm_communication'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Kevin Gopalan',
+    maintainer_email='kevin.gopalan@gmail.com',
+    description='Helper for transferring robot data to the server and VLM for data processing using rclpy',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'robot_publisher_script = vlm_communication.publisher_member_function:main',
+                'server_subscriber_script = vlm_communication.subscriber_member_function:main',
+        ],
+    },
+)
